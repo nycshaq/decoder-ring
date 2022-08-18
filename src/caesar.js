0f31a99859ae5658007088d8d53c1f61449c61b8
Shaquille <nycshaq@gmail.com>
2022-08-18 15:52:27 -0400

    updated some code and added some comments pt2

--- src/caesar.js
+++ src/caesar.js
@@ -3,43 +3,48 @@
 // Only add code (helper methods, variables, etc.) within the scope
 // of the anonymous function on line 6
 
-const caesarModule = (function () {
+const caesarModule = function () {
   // you can add any code you want within this function scope
-  
-   //create caesar = "" variable to store results
+
+  //create caesar = " " variable to store results
+
+  let caesar = " ";
 
   function caesar(input, shift, encode = true) {
     // your solution code here
-    
+
     //return false if there is no shift value, eqaul to 0, less than -25 or greater than 25
-    if(!shift || shift < -25 || shift > 25) {
-      return false
+    if (!shift || shift < -25 || shift > 25) {
+      return false;
     }
-    if(encode === false){
-      shift *= -1
+    if (encode === false) {
+      shift *= -1;
     }
-   
-    //create array of each letter in alphabet separated by commas: alphabetArray 
-    let alphabetArray = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z]
-    
+
+    //create array of each letter in alphabet separated by commas: alphabetArray
+    let alphabetArray = "abcdefghijklmnopqrstuvwxyz".split(" "); //updated
+
     //loop through alphabetArray
-    for(let index = 0; index < input.length; index ++){
+    for (let index = 0; index < input.length; index++) {
       const currentChar = input[index].toLowerCase(); //updated
-      
-       //use indexOf ? **
-      let caesarCharIndex = alphabetArray.indexOf(currentChar)
-      caesarCharIndex += shift
+
+      //use indexOf ? **
+      let caesarCharIndex = alphabetArray.indexOf(currentChar);
+      caesarCharIndex += shift;
     }
-    caesarCharIndex
-     
-   
+    if (caesarCharIndex > 25) {
+      caesarCharIndex -= 26;
+    }
+    if (caesarCharIndex < 0) {
+      caesarCharIndex += 26;
+    }
   }
 
   return {
     //maintain space separation if needed
-    
+
     caesar,
   };
-});
+};
 
 module.exports = { caesar: caesarModule.caesar };
