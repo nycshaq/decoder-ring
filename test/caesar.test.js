89455f9d308c7de0bc0dfd626d494a63fca1348a
Shaquille <nycshaq@gmail.com>
2022-08-23 17:43:49 -0400

    update caesar test

--- test/caesar.test.js
+++ test/caesar.test.js
@@ -6,6 +6,10 @@ describe("caesarModule()", () => {
   it("It should return a string containing either an encoded or decoded message based upon user input", () => {
     
     const alphabetArray = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z]
+  const expected = "wklqnixo"
+  const actual =  caesar("thinkful", 3);
+    expect(actual).to.eql(expected);
+  
   })
 
   
